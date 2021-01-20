18acbbac12af1e47f9c837278fe055286044b5d8
odin thomas rochmann <odin.rochmann@gmail.com>
2021-01-20 08:43:08 +0100

    feat: initial setup of repo

---
+++ .eslintrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+  extends: ['@equinor/eslint-config-fusion/react'],
+}
---
+++ storybook/.storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.tsx"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
