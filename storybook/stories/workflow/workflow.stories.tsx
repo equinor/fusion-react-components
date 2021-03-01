8e1531cc8a989e495d93abc916dc16fb0c143bf3
Ingrid Klepsvik <inkle@equinor.com>
2021-03-01 07:25:07 +0100

    feat: workflow component

---
+++ storybook/stories/workflow/workflow.stories.tsx
@@ -0,0 +1,8 @@
+import { Story, Meta } from '@storybook/react/types-6-0';
+/* import {RequestWorkflow} from "@equinor/fusion-workflow"; */
+
+export default {
+    title: 'Workflow/Stories',
+
+  } as Meta;
+
