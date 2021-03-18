4cd37fe2f87ed90bd46f7a72921e6a2b4c5ea3a0
odin thomas rochmann <odin.rochmann@gmail.com>
2021-03-18 12:43:35 +0100

    feat: add style generator

---
+++ packages/styles/src/index.ts
@@ -0,0 +1,5 @@
+export * from './make-styles';
+export * from './theme';
+
+export { withStyles, createStyles } from '@material-ui/styles';
+export { default as clsx } from 'clsx';
---
+++ packages/styles/src/theme.ts
@@ -0,0 +1,3 @@
+import { styles as theme } from '@equinor/fusion-web-theme';
+export type FusionTheme = typeof theme;
+export { theme };
