b32b637bfe0aee74db1b372a28b73dea821e7d35
Mats Oftedal <mats.oftedal@bouvet.no>
2021-08-13 14:17:55 +0200

    feat: added date component

---
+++ packages/date/src/DateTime.tsx
@@ -0,0 +1,20 @@
+import { FC, HTMLAttributes } from 'react';
+import { DateTimeElement, DateTimeElementProps } from '@equinor/fusion-wc-date';
+import elementAttributes from 'utils/src/element-attributes';
+export { DateTimeFormat } from '@equinor/fusion-wc-date';
+import '@equinor/fusion-wc-date';
+
+export type DateTimeProps = DateTimeElementProps &
+  HTMLAttributes<DateTimeElement> & {
+    date: Date | string;
+  };
+
+export const DateTime: FC<DateTimeProps> = (props) => {
+  const attributes = elementAttributes<DateTimeProps>(props);
+
+  console.log(attributes);
+
+  return <fwc-datetime {...attributes}></fwc-datetime>;
+};
+
+export default DateTime;
