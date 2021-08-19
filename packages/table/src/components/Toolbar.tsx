f56891e259d0c72f86fc04f0da1cb839c9522ff8
Joachim <joachim290@outlook.com>
2021-08-19 10:42:36 +0200

    chore: add justify styling option for toolbar

--- packages/table/src/components/Toolbar.tsx
+++ packages/table/src/components/Toolbar.tsx
@@ -1,22 +1,29 @@
 import Button from '@equinor/fusion-react-button';
-import { createStyles, makeStyles } from '@equinor/fusion-react-styles';
+import { createStyles, FusionTheme, makeStyles } from '@equinor/fusion-react-styles';
 import { ReactNode } from 'react';
 
 type ToolbarProps = {
   disableExport?: boolean;
+  justify?: 'flex-start' | 'center' | 'flex-end';
   children?: ReactNode | undefined;
 };
-
-const useStyles = makeStyles(() =>
+type StyleProps = {
+  container: {
+    justifyContent: string;
+  };
+};
+const useStyles = makeStyles<FusionTheme, StyleProps>(() =>
   createStyles({
-    container: {
+    container: ({ container }) => ({
       display: 'flex',
       flexDirection: 'row',
-    },
+      gap: '1em',
+      ...container,
+    }),
   })
 );
-export const Toolbar = ({ disableExport = false, children }: ToolbarProps): JSX.Element => {
-  const styles = useStyles();
+export const Toolbar = ({ disableExport = false, justify = 'flex-start', children }: ToolbarProps): JSX.Element => {
+  const styles = useStyles({ container: { justifyContent: justify } });
   return (
     <div className={styles.container}>
       {!disableExport && <Button>Export to Excel</Button>}
