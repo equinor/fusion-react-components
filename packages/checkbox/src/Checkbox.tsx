a752f4f64616d365f8046d9ee213c3373c3964c3
odin thomas rochmann <odin.rochmann@gmail.com>
2021-09-06 09:22:48 +0200

    fix: add label to checkbox

--- packages/checkbox/src/Checkbox.tsx
+++ packages/checkbox/src/Checkbox.tsx
@@ -1,11 +1,38 @@
-import { HTMLAttributes } from 'react';
-import CheckboxElement, { CheckboxElementProps } from '@equinor/fusion-wc-checkbox/lib/element';
+import { ChangeEvent, ChangeEventHandler, FormEvent, HTMLAttributes, useCallback } from 'react';
+import { CheckboxElement, CheckboxElementProps } from '@equinor/fusion-wc-checkbox';
+
+import { elementAttributes } from '@equinor/fusion-react-utils';
+
 import '@equinor/fusion-wc-checkbox';
+import { FormfieldElementProps } from '@equinor/fusion-wc-formfield';
 
-export type CheckboxProps = CheckboxElementProps & HTMLAttributes<CheckboxElement>;
+// TODO add  prop for label and wrap element inside
 
-export const Checkbox = ({ ...rest }: CheckboxProps): JSX.Element => {
-  return <fwc-checkbox {...rest}></fwc-checkbox>;
+export type TextInputChangeHandler = ChangeEventHandler<CheckboxElement>;
+export type TextInputChangeEvent = ChangeEvent<CheckboxElement>;
+
+export type CheckboxProps = CheckboxElementProps &
+  HTMLAttributes<CheckboxElement> & {
+    onChange?: TextInputChangeHandler;
+    label?: string;
+    formfield?: FormfieldElementProps;
+  };
+
+export const Checkbox = ({ onChange, ...props }: CheckboxProps): JSX.Element => {
+  const { formfield = {}, label, ...attr } = elementAttributes(props as CheckboxProps);
+  formfield.label = label;
+  const onInput = useCallback(
+    ({ nativeEvent, currentTarget: target }: FormEvent<CheckboxElement>) => {
+      // @ts-ignore
+      onChange && onChange({ nativeEvent, target });
+    },
+    [onChange]
+  );
+  return (
+    <fwc-formfield {...formfield}>
+      <fwc-checkbox {...attr} onInput={onInput} />
+    </fwc-formfield>
+  );
 };
 
 export default Checkbox;
---
+++ storybook/stories/checkbox/CheckBoxTemplate.tsx
@@ -0,0 +1,5 @@
+import { Checkbox, CheckboxProps } from '@equinor/fusion-react-checkbox';
+
+export const CheckboxTemplate = (props: CheckboxProps): JSX.Element => <Checkbox {...props} />;
+
+export default CheckboxTemplate;
