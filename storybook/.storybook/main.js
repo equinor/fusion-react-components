f8edef9697438e69b5e19282751cfea8e7a0520f
odin thomas rochmann <odin.rochmann@gmail.com>
2021-09-21 14:39:50 +0200

    feat(storybook): update to webpack 5
    + make chromatic explisive
    + set doc tab only as default
    - remove css from arg types

--- storybook/.storybook/main.js
+++ storybook/.storybook/main.js
@@ -1,8 +1,31 @@
+const path = require('path');
+
 module.exports = {
-  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.tsx'],
+  core: {
+    builder: "webpack5",
+  },
+  // stories: ['../stories/**/*.mdx', '../stories/**/*.stories.tsx', '../../packages/*/doc/*.stories.tsx'],
+  // stories: ['../../packages/*/doc/*.stories.tsx'],
+  stories: ['../stories/**/*.stories.{tsx,mdx}'],
+    typescript: {
+      check: false,
+      checkOptions: {},
+      reactDocgen: 'react-docgen-typescript',
+      reactDocgenTypescriptOptions: {
+        tsconfigPath: path.resolve(__dirname, '../tsconfig.js'),
+        shouldExtractLiteralValuesFromEnum: true,
+        propFilter: (prop) => (prop.parent ? !prop.parent.fileName.includes('@types/react') : true),
+      },
+    },
   addons: [
-    '@storybook/addon-docs',
-    '@storybook/addon-essentials',
-    '@storybook/addon-actions',
-  ],
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        configureJSX: true,
+        babelOptions: {},
+        sourceLoaderOptions: null,
+      }
+    },
+    '@storybook/addon-storysource'
+  ]
 };
--- storybook/.storybook/preview.js
+++ storybook/.storybook/preview.js
@@ -1,7 +1,19 @@
 import { ThemeProvider } from '@equinor/fusion-react-styles';
 
+import './theme.css';
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
+  viewMode: 'docs',
+  previewTabs: {
+    canvas: { hidden: true },
+  },
+  chromatic: { 
+    disableSnapshot: true 
+  },
+  argTypes:{
+    css: {control: {disable: true}}
+  }
 };
 
 export const decorators = [
