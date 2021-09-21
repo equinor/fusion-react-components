67045e5d9bb0c089b5b53f7cd9dfed334d31c042
odin thomas rochmann <odin.rochmann@gmail.com>
2021-09-21 14:42:40 +0200

    docs(textinput): update stories

--- storybook/stories/textinput/TextInput.template.tsx
+++ storybook/stories/textinput/TextInput.template.tsx
@@ -1,5 +1,5 @@
 import TextInput, { TextInputProps } from '@equinor/fusion-react-textinput';
 
-export const Template = (props: TextInputProps): JSX.Element => <TextInput {...props} />;
+export const Template = (props: TextInputProps): React.ReactElement => <TextInput {...props} />;
 
 export default Template;
