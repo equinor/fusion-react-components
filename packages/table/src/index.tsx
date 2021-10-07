adb72d2406bd339867065867781d35d24c637c1d
Joachim <joachim290@outlook.com>
2021-10-07 11:32:33 +0200

    chore: remove old pagination layout

--- packages/table/src/index.tsx
+++ packages/table/src/index.tsx
@@ -1,6 +1,5 @@
 export * from './useTable';
 export * from './components';
 export * from './types';
-export { PaginationLayout } from './layout';
 export * from './Table';
 export * from './TableProvider';
