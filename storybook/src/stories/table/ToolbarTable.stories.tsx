4ab45814ad3c6c10f25454164216743c62d0c015
Joachim <joachim290@outlook.com>
2021-10-11 08:51:27 +0200

    chore: change storybook component for table with toolbar

---
+++ storybook/src/stories/table/ToolbarTable.stories.tsx
@@ -0,0 +1,61 @@
+import { useMemo } from 'react';
+import { Toolbar } from 'table/src/components';
+import Table from 'table/src/Table';
+import { Column } from 'table/src/types';
+import makeData from './makeData';
+const columns: Column[] = [
+  {
+    Header: 'First Name',
+    accessor: 'firstName',
+  },
+  {
+    Header: 'Last Name',
+    accessor: 'lastName',
+  },
+  {
+    Header: 'Age',
+    accessor: 'age',
+  },
+  {
+    Header: 'Visits',
+    accessor: 'visits',
+  },
+  {
+    Header: 'Status',
+    accessor: 'status',
+  },
+  {
+    Header: 'Profile Progress',
+    accessor: 'progress',
+  },
+];
+type StoryProps = { rows: number };
+export const ToolbarTable = ({ rows }: StoryProps) => {
+  const options = useMemo(
+    () => ({
+      data: makeData(rows),
+      columns,
+      exportFn: async (_: any) => {
+        await new Promise((resolve) => setTimeout(resolve, 10000));
+        return { url: 'https://vg.no', fileName: 'storybook' };
+      },
+    }),
+    [rows]
+  );
+
+  return <Table options={options} slots={{ Toolbar: <Toolbar /> }}></Table>;
+};
+
+export default {
+  title: 'Table/Examples/Toolbar Table',
+  component: ToolbarTable,
+  parameters: {
+    viewMode: 'docs',
+    previewTabs: {
+      canvas: { hidden: true },
+    },
+    chromatic: {
+      disableSnapshot: false,
+    },
+  },
+};
