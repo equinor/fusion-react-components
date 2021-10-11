d248d2b973a1cd4a18401b5ea719d5778957a563
Joachim <joachim290@outlook.com>
2021-10-11 08:51:15 +0200

    chore: change storybook component for table with toolbar

--- storybook/src/stories/table/BasicTable.stories.tsx
+++ storybook/src/stories/table/BasicTable.stories.tsx
@@ -1,7 +1,6 @@
-import { Column, Table, Toolbar } from '@equinor/fusion-react-table';
-import { Meta, Story } from '@storybook/react/types-6-0';
+import { Column, Table, useTableContext } from '@equinor/fusion-react-table';
+import { Meta } from '@storybook/react';
 import { useMemo } from 'react';
-import Button from '@equinor/fusion-react-button';
 import makeData from './makeData';
 
 export default {
@@ -20,78 +19,52 @@ const columns: Column[] = [
   {
     Header: 'First Name',
     accessor: 'firstName',
-    dataType: 'text',
   },
   {
     Header: 'Last Name',
     accessor: 'lastName',
-    dataType: 'text',
   },
   {
     Header: 'Age',
     accessor: 'age',
-    dataType: 'number',
   },
   {
     Header: 'Visits',
     accessor: 'visits',
-    dataType: 'number',
   },
   {
     Header: 'Status',
     accessor: 'status',
-    dataType: 'text',
   },
   {
     Header: 'Profile Progress',
     accessor: 'progress',
-    dataType: 'number',
   },
 ];
-const ToolbarChildren = () => {
-  return (
-    <div style={{ display: 'flex', gap: '1em' }}>
-      <Button>Select</Button>
-      <Button>Example</Button>
-    </div>
-  );
+
+const Debugger = () => {
+  const { instance } = useTableContext();
+
+  return <pre>{JSON.stringify(instance.state, null, 2)}</pre>;
 };
-type Props = {
-  rows: number;
-  justifyContent: 'flex-start' | 'center' | 'flex-end';
-  toolbarChildren?: JSX.Element;
-};
-const Template: Story<Props> = (args) => {
+
+// TODO - generate data
+const Template = ({ rows }: { rows: number }) => {
   const options = useMemo(
     () => ({
-      data: makeData(args.rows),
+      data: makeData(rows),
       columns,
-      exportFn: async (data: any) => {
-        await new Promise((resolve) => setTimeout(resolve, 10000));
-        return { url: 'https://vg.no', fileName: 'storybook' };
-      },
     }),
-    [args.rows]
+    [rows]
   );
-
   return (
-    <Table
-      options={options}
-      slots={{
-        Toolbar: <Toolbar children={args.toolbarChildren} justify={args.justifyContent} hideExportBtn={false} />,
-      }}
-    ></Table>
+    <Table options={options} slots={{ Toolbar: null }} style={{ minWidth: '100%' }}>
+      <Debugger />
+    </Table>
   );
 };
 
 export const BasicTable = Template.bind({});
 BasicTable.args = {
   rows: 10,
-};
-
-export const CustomTableToolbar = Template.bind({});
-CustomTableToolbar.args = {
-  rows: 10,
-  justifyContent: 'flex-end',
-  toolbarChildren: <ToolbarChildren />,
 };
