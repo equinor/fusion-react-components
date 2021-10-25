dfb853c1d99edf1ceec7fcbd91e9731c65c44ef1
odin thomas rochmann <odin.rochmann@gmail.com>
2021-10-25 14:09:05 +0200

    feat(filter): add hook for attaching filter to context

---
+++ packages/filter/src/hooks/useFilter.ts
@@ -0,0 +1,33 @@
+import { useCallback, useLayoutEffect } from 'react';
+
+import { actions } from '../actions';
+import { useFilterContext } from '../context';
+import { Filter } from '../types';
+
+/**
+ * Hook for attaching a filter to the current context
+ * Must be used within {@link FilterProvider}
+ */
+export const useFilter = <TData, TSelection>(filter: Filter<TData, TSelection>): ((value: TSelection) => void) => {
+  const context = useFilterContext();
+  const { key } = filter;
+  useLayoutEffect(() => {
+    /** add filter to store */
+    context.filter$.next(actions.filter.add(filter));
+
+    /** add initial selection to filter selection */
+    if (!Object.keys(context.selection$.value).includes(key)) {
+      context.selection$.next(actions.selection.initial({ [key]: filter.initial }));
+    }
+    /** remove filter when consumer unmounts */
+    return () => context.filter$.next(actions.filter.remove(filter.key));
+  }, [context, filter, key]);
+
+  /** return a function which the consumer can set the selection for the provided filter  */
+  return useCallback(
+    (selection: TSelection) => {
+      context.selection$.next(actions.selection.set({ [key]: selection }));
+    },
+    [context, key]
+  );
+};
