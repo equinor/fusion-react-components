a005e61671d9a559e536610cbed73d534072b7c6
odin thomas rochmann <odin.rochmann@gmail.com>
2021-10-28 11:13:58 +0200

    fix(filter): make checkbox counter no wrap

--- packages/filter/src/components/filter/checkbox/CheckboxFilterOption.style.ts
+++ packages/filter/src/components/filter/checkbox/CheckboxFilterOption.style.ts
@@ -33,6 +33,7 @@ export const useStyles = makeStyles(
       },
       counter: {
         ...theme.typography.input.label.style,
+        whiteSpace: 'nowrap',
         margin: `0 ${theme.spacing.comfortable.small.getVariable('padding')}`,
       },
       hide: {
