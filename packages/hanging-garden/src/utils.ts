4b23dfd30d2ff5c4df8301a478e59273184b5ff3
Joachim <joachim290@outlook.com>
2021-10-29 15:49:20 +0200

    feat: add new scrolling hook and utils

--- packages/hanging-garden/src/utils.ts
+++ packages/hanging-garden/src/utils.ts
@@ -177,3 +177,46 @@ PIXI.Graphics.prototype.drawDashLine = function (this: PIXI.Graphics, toX: numbe
   }
   this.lineTo(to.x, to.y);
 };
+
+/**
+ * Function for extracting the longest description of packages inside one column
+ * @param columnData One column from the garden
+ * @returns Longest description string
+ */
+export const getLongestDescription = <T extends object>(columnData: T[]) => {
+  let longest = '';
+  columnData.map((e: any) => (e.description.length > longest.length ? (longest = e.description) : null));
+  return longest;
+};
+/**
+ * Function for estimating the extra width it takes to display the descriptions of packages.
+ * @param columnData One column from the garden
+ * @returns A calculated width based on string length and font
+ */
+export const getDescriptionWidth = <T extends object>(columnData: T[]): number => {
+  const longestDescription = getLongestDescription(columnData);
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext('2d');
+  ctx!.font = '16px Equinor, sans-serif';
+  const width = ctx!.measureText(longestDescription).width;
+  return width;
+};
+export function cancelTimeout(handle: { id: number | null }) {
+  handle && handle.id && cancelAnimationFrame(handle.id);
+}
+export function requestTimeout(fn: () => void, delay: number) {
+  const start = Date.now();
+
+  function loop() {
+    if (Date.now() - start >= delay) {
+      fn.call(null);
+    } else {
+      handle.id = requestAnimationFrame(loop);
+    }
+  }
+  const handle = {
+    id: requestAnimationFrame(loop),
+  };
+
+  return handle;
+}
