ed50666314627f90bc198aaca537cefbe8ec2ecc
Joachim <joachim290@outlook.com>
2021-11-01 08:53:16 +0100

    chore: add type guard for description

--- packages/hanging-garden/src/utils.ts
+++ packages/hanging-garden/src/utils.ts
@@ -178,6 +178,18 @@ PIXI.Graphics.prototype.drawDashLine = function (this: PIXI.Graphics, toX: numbe
   this.lineTo(to.x, to.y);
 };
 
+type PackageWithDescription<T extends object> = T & {
+  description: string;
+};
+/** Type guard for a package object checking whether or not it contains a description property */
+function packageHasDescription<T extends object>(packageObj: T): packageObj is PackageWithDescription<T> {
+  if ((packageObj as PackageWithDescription<T>).description) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 /**
  * Function for extracting the longest description of packages inside one column
  * @param columnData One column from the garden
@@ -185,7 +197,10 @@ PIXI.Graphics.prototype.drawDashLine = function (this: PIXI.Graphics, toX: numbe
  */
 export const getLongestDescription = <T extends object>(columnData: T[]) => {
   let longest = '';
-  columnData.map((e: any) => (e.description.length > longest.length ? (longest = e.description) : null));
+
+  columnData.forEach((e: T) =>
+    packageHasDescription(e) && e.description.length > longest.length ? (longest = e.description) : null
+  );
   return longest;
 };
 /**
@@ -201,6 +216,7 @@ export const getDescriptionWidth = <T extends object>(columnData: T[]): number =
   const width = ctx!.measureText(longestDescription).width;
   return width;
 };
+
 export function cancelTimeout(handle: { id: number | null }) {
   handle && handle.id && cancelAnimationFrame(handle.id);
 }
