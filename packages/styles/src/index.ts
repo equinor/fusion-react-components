23d9c8a9d3831e5beca9b5ff58e951bd1ba1e809
odin thomas rochmann <odin.rochmann@gmail.com>
2021-11-23 11:12:51 +0100

    fix(style): fix create-style properties

--- packages/styles/src/index.ts
+++ packages/styles/src/index.ts
@@ -7,3 +7,5 @@ export { withStyles, Styles, createGenerateClassName } from '@material-ui/styles
 export { default as clsx } from 'clsx';
 export { StylesProvider } from './StyleProvider';
 export { ThemeProvider } from './ThemeProvider';
+
+export type { StyleRules, StyleCSSProperties } from './types';
