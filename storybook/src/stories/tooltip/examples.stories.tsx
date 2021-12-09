3335a6efb065588a572c0dce3b70971d6c6905e7
Silje Stangeland <scst@equinor.com>
2021-12-09 16:59:15 +0100

    feat(tooltip): Stories for Custom + Styled Tooltip

--- storybook/src/stories/tooltip/examples.stories.tsx
+++ storybook/src/stories/tooltip/examples.stories.tsx
@@ -1,28 +1,76 @@
 import { Meta, Story } from '@storybook/react';
-import Tooltip from '@equinor/fusion-react-tooltip/src';
-// import { createStyles } from '@/create-styles';
+import Tooltip, { TooltipProps } from '@equinor/fusion-react-tooltip/src';
+import 'tippy.js/dist/tippy.css';
+import { createStyles, makeStyles } from '@equinor/fusion-react-styles';
+import Button from '@equinor/fusion-react-button';
 
 export default {
   title: 'Examples/Tooltip',
-  // component: Test,
+  component: Tooltip,
 } as Meta;
 
-// export const Testing: Story = () => <p>ok</p>;
-export const Testings: Story = (_args: any) => (
-  <>
-    <Tooltip content="test">
-      <button>testing</button>
-    </Tooltip>
-    <Tooltip
-      render={(_args) => (
-        <div>
-          <p>ok</p>
-          <span>dsadasds</span>
-        </div>
-      )}
-      duration={[100, 1000]}
-    >
-      <button>testing</button>
-    </Tooltip>
-  </>
+type Props = Omit<TooltipProps, 'ref'>;
+
+export const Component: Story = (props: Props) => (
+  <Tooltip {...props}>
+    <button>Hover Me!</button>
+  </Tooltip>
 );
+Component.args = {
+  content: 'Tooltip',
+};
+
+export const Custom: Story = (props: Props) => (
+  <Tooltip {...props}>
+    <span>Hover and Interact With Me!</span>
+  </Tooltip>
+);
+Custom.args = {
+  interactive: true,
+  appendTo: () => document.body,
+  content: (
+    <div>
+      <h3>Lorem ipsum dolor sit amet</h3>
+      <p>
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+        sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <Button>Learn More</Button>
+      <h5>Ut enim ad minim veniam.</h5>
+    </div>
+  ),
+};
+
+const useStyles = makeStyles(
+  (theme) =>
+    createStyles({
+      styled: {
+        color: theme.colors.infographic.primary__slate_blue.getVariable('color'),
+        backgroundColor: theme.colors.infographic.primary__spruce_wood.getVariable('color'),
+        borderRadius: '0',
+        ...theme.typography.input.text_monospaced.style,
+        '& > .tippy-arrow': {
+          color: theme.colors.infographic.primary__spruce_wood.getVariable('color'),
+        },
+      },
+    }),
+  { name: 'fusion-tooltip' }
+);
+export const Styled: Story = (props: Props) => {
+  const styles = useStyles();
+  return (
+    <Tooltip
+      {...props}
+      className={styles.styled}
+      placement={'right'}
+      visible
+      content={
+        <div>
+          Tooltip with <b>custom style</b>
+        </div>
+      }
+    >
+      <span>Pssst!</span>
+    </Tooltip>
+  );
+};
