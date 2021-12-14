4c53c9f4b4747579b2689bba44f2b9ea10699c21
Silje Stangeland <scst@equinor.com>
2021-12-14 16:11:50 +0100

    feat(tooltip): container style for fusion tooltip

--- packages/tooltip/src/index.tsx
+++ packages/tooltip/src/index.tsx
@@ -4,8 +4,12 @@ import { useStyles } from './style';
 export type TooltipProps = TippyProps;
 
 export const Tooltip = (props: TippyProps): JSX.Element => {
-  useStyles();
-  return <Tippy {...props}>{props.children}</Tippy>;
+  const styles = useStyles();
+  return (
+    <Tippy className={styles.container} {...props}>
+      {props.children}
+    </Tippy>
+  );
 };
 
 export default Tooltip;
