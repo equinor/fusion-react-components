be1660525a9873ba6968d472aa5e1c821e63a6e3
Silje Stangeland <scst@equinor.com>
2021-12-17 16:11:23 +0100

    feat(tooltip): made tooltip headless

--- packages/tooltip/src/index.tsx
+++ packages/tooltip/src/index.tsx
@@ -1,12 +1,21 @@
-import Tippy, { TippyProps } from '@tippyjs/react';
+import Tippy, { TippyProps } from '@tippyjs/react/headless';
 import { useStyles } from './style';
 
 export type TooltipProps = TippyProps;
 
-export const Tooltip = (props: TippyProps): JSX.Element => {
+export const Tooltip = (props: TooltipProps): JSX.Element => {
   const styles = useStyles();
+
   return (
-    <Tippy className={styles.container} {...props}>
+    <Tippy
+      render={(attrs) => (
+        <div className={styles.box} {...attrs}>
+          <div className={styles.arrow} data-popper-arrow></div>
+          <div className={styles.content}>{props.content}</div>
+        </div>
+      )}
+      {...props}
+    >
       {props.children}
     </Tippy>
   );
--- storybook/src/stories/tooltip/examples.stories.tsx
+++ storybook/src/stories/tooltip/examples.stories.tsx
@@ -1,75 +1,62 @@
 import { Meta, Story } from '@storybook/react';
-import Tooltip, { TooltipProps } from '@equinor/fusion-react-tooltip/src';
+import Tooltip, { TooltipProps } from '@equinor/fusion-react-tooltip/src/';
 import { createStyles, makeStyles } from '@equinor/fusion-react-styles';
-import Button from '@equinor/fusion-react-button';
 
 export default {
   title: 'Examples/Tooltip',
   component: Tooltip,
 } as Meta;
 
-type Props = Omit<TooltipProps, 'ref'>;
-
-export const Component: Story = (props: Props) => (
-  <Tooltip {...props}>
+export const Component: Story = (props: TooltipProps) => (
+  <Tooltip {...props}
+    content="Default Tooltip"
+  >
     <button>Hover Me!</button>
   </Tooltip>
 );
-Component.args = {
-  content: 'Tooltip with default styling',
-};
 
-export const Custom: Story = (props: Props) => (
-  <Tooltip {...props}>
-    <span>Hover and Interact With Me!</span>
+export const Default: Story = (props: TooltipProps) => (
+  <Tooltip {...props}
+    content="Default Tooltip"
+  >
+    <button>Hover Me!</button>
   </Tooltip>
 );
-Custom.args = {
-  interactive: true,
-  appendTo: () => document.body,
-  content: (
-    <div>
-      <h3>Lorem ipsum dolor sit amet</h3>
-      <p>
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-        sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
-      <Button>Learn More</Button>
-      <h5>Ut enim ad minim veniam.</h5>
-    </div>
-  ),
-};
+
+export const Interactive: Story = (props: TooltipProps) => (
+  <Tooltip {...props}
+    content={<div>You could copy this text if you wanted to</div>}
+    interactive
+    appendTo={() => document.body}
+  >
+    <button>Hover Me!</button>
+  </Tooltip>
+);
 
 const useStyles = makeStyles(
   (theme) =>
     createStyles({
-      styled: {
+      tooltip: {
+        ...theme.typography.input.text_monospaced.style,
         color: theme.colors.infographic.primary__slate_blue.getVariable('color'),
         backgroundColor: theme.colors.infographic.primary__spruce_wood.getVariable('color'),
-        borderRadius: '0',
-        ...theme.typography.input.text_monospaced.style,
-        '& > .tippy-arrow': {
-          color: theme.colors.infographic.primary__spruce_wood.getVariable('color'),
-        },
+        padding: '10px',
       },
     }),
-  { name: 'fusion-tooltip' }
+  { name: 'fusion-styled-tooltip' }
 );
-export const Styled: Story = (props: Props) => {
+export const Styled: Story = (props: TooltipProps) => {
   const styles = useStyles();
   return (
     <Tooltip
       {...props}
-      className={styles.styled}
-      placement={'right'}
-      visible
-      content={
-        <div>
+      render={attrs => (
+        <div {...attrs} className={styles.tooltip}>
           Tooltip with <b>custom style</b>
         </div>
-      }
+      )}
     >
-      <span>Pssst!</span>
+      <span>Hover Me!</span>
     </Tooltip>
   );
 };
