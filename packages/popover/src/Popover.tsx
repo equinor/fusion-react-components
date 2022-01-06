ca556f9fd419024052371dd02a8dff6df8ebe073
Silje Stangeland <scst@equinor.com>
2022-01-06 16:27:39 +0100

    feat(popover): default popper popover

--- packages/popover/src/Popover.tsx
+++ packages/popover/src/Popover.tsx
@@ -1,9 +1,39 @@
-export type PopoverProps = { test?: boolean };
+import { useState } from 'react';
+import { usePopper, PopperProps, PopperChildrenProps } from 'react-popper';
+
+export type ChildrenProps = PopperChildrenProps;
+export type PopoverProps = PopperProps<any>;
 
 export const Popover = (props: PopoverProps): JSX.Element => {
+  const [visible, setVisibility] = useState(false);
+  const [referenceElement, setReferenceElement] = useState<HTMLSpanElement | null>(null);
+  const [popperElement, setPopperElement] = useState<HTMLDivElement | null>(null);
+  const [arrowElement, setArrowElement] = useState<HTMLDivElement | null>(null);
+  const { styles, attributes } = usePopper(referenceElement, popperElement, {
+    placement: props.placement,
+    modifiers: [
+      { name: 'arrow', options: { element: arrowElement } },
+      //   { name: 'preventOverflow', enabled: false },
+    ],
+  });
+
+  const handleClick = () => {
+    setVisibility(!visible);
+  };
+
   return (
     <div>
-      Popover <span>{props.test}</span>
+      <span ref={setReferenceElement} onClick={handleClick}>
+        Reference element
+      </span>
+      {visible && (
+        <div ref={setPopperElement} style={styles.popper} {...attributes.popper}>
+          Popper element
+          {props.children}
+          <span onClick={handleClick}>Close</span>
+          <div ref={setArrowElement} style={styles.arrow} />
+        </div>
+      )}
     </div>
   );
 };
--- storybook/src/stories/popover/component.stories.tsx
+++ storybook/src/stories/popover/component.stories.tsx
@@ -1,21 +1,46 @@
 import { Meta, Story } from '@storybook/react';
-import { Popover, PopoverProps } from '@equinor/fusion-react-popover/src';
+import { Popover, PopoverProps, ChildrenProps } from '@equinor/fusion-react-popover/src';
+
+const Children = (): JSX.Element => {
+  return <div>Child Component</div>;
+};
 
 export default {
   title: 'Examples/Popover',
   component: Popover,
+  argTypes: {
+    children: {
+      table: {
+        type: {
+          summary: 'React.ReactNode',
+        },
+      },
+      control: {
+        type: 'select',
+        options: {
+          default: [<Children key="1" />],
+        },
+      },
+    },
+  },
 } as Meta;
 
-const Template: Story<PopoverProps> = (args) => <Popover {...args} />;
+const Template: Story<PopoverProps> = (args) => <Popover {...args}>{args.children}</Popover>;
 
 export const Component = Template.bind({});
 
 Component.args = {
-  test: true,
+  placement: 'top',
 };
-
-export const Title = Template.bind({});
-
-Title.args = {
-  test: false,
-};
+const Content = (props?: ChildrenProps) => (
+  <div {...props}>
+    <button>button1</button>
+    <button>button2</button>
+  </div>
+);
+const Template2: Story<PopoverProps> = (args) => (
+  <Popover {...args}>
+    <Content />
+  </Popover>
+);
+export const Simple = Template2.bind({});
