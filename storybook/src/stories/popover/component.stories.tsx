bfe223433f3e6b82e95c8e1518ff57e87389212b
Silje Stangeland <scst@equinor.com>
2022-01-20 15:01:32 +0100

    feat(popover): visibility controlled from outside
    Co-authored-by: Eskil Lindløv Sand <eskil0312@users.noreply.github.com>

--- storybook/src/stories/popover/component.stories.tsx
+++ storybook/src/stories/popover/component.stories.tsx
@@ -1,44 +1,43 @@
 import { Meta, Story } from '@storybook/react';
 import { Popover, PopoverProps } from '@equinor/fusion-react-popover/src';
+import { useState } from 'react';
 
 export default {
   title: 'Examples/Popover',
   component: Popover,
-  argTypes: {
-    children: {
-      table: {
-        type: {
-          summary: '(props: PopperChildrenProps) => React.ReactNode',
-        },
-      },
-    },
-  },
 } as Meta;
 
-const Template: Story<PopoverProps> = (args) => (
-  <Popover {...args}>
-    <div>Child Element</div>
-  </Popover>
-);
+const Template: Story<PopoverProps> = (args) => {
+  const [visible, setVisibility] = useState<boolean>(false);
+  return (
+    <Popover {...args} setVisibility={setVisibility} visible={visible}>
+      <div>Child Element</div>
+    </Popover>
+  );
+};
 
 export const Component = Template.bind({});
 Component.args = {
-  placement: 'auto',
   strategy: 'fixed',
   baseElement: <span>Base Element</span>,
   title: <div>Title Element</div>,
 };
 
-const Template2: Story<PopoverProps> = (args) => (
-  <Popover {...args}>
-    <div>
-      <button>button1</button>
-      <button>button2</button>
-    </div>
-  </Popover>
-);
+const Template2: Story<PopoverProps> = (args) => {
+  const [visible, setVisibility] = useState<boolean>(false);
+
+  return (
+    <Popover {...args} setVisibility={setVisibility} visible={visible}>
+      <div>
+        <button>button1</button>
+        <button>button2</button>
+      </div>
+    </Popover>
+  );
+};
 export const Simple = Template2.bind({});
 Simple.args = {
   strategy: 'fixed',
+  showCloseIcon: false,
   baseElement: <span>Base Element</span>,
 };
