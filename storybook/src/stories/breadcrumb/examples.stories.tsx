c92d632b54abca5898f4cf6df478f66b8c27c3cb
Silje Stangeland <scst@equinor.com>
2022-02-14 08:47:53 +0100

    fix(breadcrumb): fixed fetching story
    set currentLevel on stories

--- storybook/src/stories/breadcrumb/examples.stories.tsx
+++ storybook/src/stories/breadcrumb/examples.stories.tsx
@@ -20,26 +20,22 @@ const breadcrumbs: BreadcrumbItemProps[] = [
   { link: '/link-to-kingdom', name: 'Kingdom', isActive: false },
   { link: '/link-to-phylum', name: 'Phylum', isActive: false },
   { link: '/link-to-class', name: 'Class', isActive: true },
-  // { link: '/link-to-order', name: 'Order' },
-  // { link: '/link-to-family', name: 'Family' },
-  // { link: '/link-to-genus', name: 'Genus' },
-  // { link: '/link-to-species', name: 'Species', isActive: true },
 ];
 
 export const Component: Story<BreadcrumbProps> = (props: BreadcrumbProps) => (
-  <Breadcrumb {...props} breadcrumbs={breadcrumbs} currentLevel={1} />
+  <Breadcrumb {...props} breadcrumbs={breadcrumbs} />
 );
 
-export const Simple: Story = () => <Breadcrumb isFetching={false} breadcrumbs={breadcrumbs} />;
+export const Simple: Story = () => <Breadcrumb currentLevel={3} isFetching={false} breadcrumbs={breadcrumbs} />;
 
 export const Colored: Story = () => (
   <div style={{ color: 'salmon' }}>
-    <Breadcrumb isFetching={false} breadcrumbs={breadcrumbs} />
+    <Breadcrumb isFetching={false} currentLevel={3} breadcrumbs={breadcrumbs} />
   </div>
 );
 
 export const Fetching: Story = () => (
   <div>
-    <Breadcrumb breadcrumbs={breadcrumbs} isFetching={true} />
+    <Breadcrumb isFetching={true} currentLevel={3} breadcrumbs={breadcrumbs} />
   </div>
 );
