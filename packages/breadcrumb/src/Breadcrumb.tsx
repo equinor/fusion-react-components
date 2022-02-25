9f71669b4c4ddd3e7b2f714b0e5b1b8026fb5164
Silje Stangeland <scst@equinor.com>
2022-02-25 10:03:19 +0100

    fix(breadcrumb): export breadcrumb item props

--- packages/breadcrumb/src/Breadcrumb.tsx
+++ packages/breadcrumb/src/Breadcrumb.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import BreadcrumbItem from './components/BreadcrumbItem';
 import Divider from './components/Divider';
-import { BreadcrumbItemProps, BreadcrumbProps as Props } from './types';
+import { BreadcrumbItemProps as ItemProps, BreadcrumbProps as Props } from './types';
 import Skeleton, { SkeletonSize, SkeletonVariant } from '@equinor/fusion-react-skeleton';
 import { createStyles, makeStyles } from '@equinor/fusion-react-styles';
 
@@ -31,29 +31,29 @@ export const Breadcrumb: FC<Props> = ({ breadcrumbs, isFetching, currentLevel, h
 
   return (
     <div className={style.breadcrumbContainer}>
-      {(isFetching ? levelArray : breadcrumbs)?.map(
-        (crumb: BreadcrumbItemProps, index: number, array: BreadcrumbItemProps[]) => {
-          const lastCrumb = array.length - 1 === index;
-          return (
-            <div className={style.crumb} key={`${crumb.name}-${index.toString()}`}>
-              {isFetching ? (
-                <Skeleton variant={SkeletonVariant.Text} size={SkeletonSize.XSmall} />
-              ) : (
-                <BreadcrumbItem
-                  onClick={crumb.onClick}
-                  name={crumb.name}
-                  isActive={crumb.isActive || lastCrumb}
-                  hasHoverColor={hasHoverColor}
-                />
-              )}
-              {!lastCrumb && <Divider />}
-            </div>
-          );
-        }
-      )}
+      {(isFetching ? levelArray : breadcrumbs)?.map((crumb: ItemProps, index: number, array: ItemProps[]) => {
+        const lastCrumb = array.length - 1 === index;
+        return (
+          <div className={style.crumb} key={`${crumb.name}-${index.toString()}`}>
+            {isFetching ? (
+              <Skeleton variant={SkeletonVariant.Text} size={SkeletonSize.XSmall} />
+            ) : (
+              <BreadcrumbItem
+                onClick={crumb.onClick}
+                name={crumb.name}
+                isActive={crumb.isActive || lastCrumb}
+                hasHoverColor={hasHoverColor}
+              />
+            )}
+            {!lastCrumb && <Divider />}
+          </div>
+        );
+      })}
     </div>
   );
 };
+
+export type BreadcrumbItemProps = React.ComponentProps<typeof BreadcrumbItem>;
 
 export type BreadcrumbProps = React.ComponentProps<typeof Breadcrumb>;
 
