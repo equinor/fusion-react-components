503386114e71aec88961a7187c7acf85f388b618
Silje Stangeland <scst@equinor.com>
2022-04-06 10:51:46 +0200

    feat(select): create select
    initial commit

---
+++ packages/select/README.md
@@ -0,0 +1,16 @@
+<!--prettier-ignore-start-->
+# @equinor/fusion-react-select
+
+[![Published on npm](https://img.shields.io/npm/v/@equinor/fusion-react-menu.svg)](https://www.npmjs.com/package/@equinor/fusion-react-select)
+
+[Storybook](https://equinor.github.io/fusion-react-components/?path=/docs/input-select)
+
+[Fusion Web Component](https://github.com/equinor/fusion-web-components/tree/main/packages/select)
+
+## Installation
+
+```sh
+npm install @equinor/fusion-react-select
+```
+
+<!--prettier-ignore-end-->
---
+++ storybook/src/stories/select/examples.stories.tsx
@@ -0,0 +1,20 @@
+import { Meta, Story } from '@storybook/react';
+import { Select, SelectProps } from '@equinor/fusion-react-select/src';
+import { ListItem } from '@equinor/fusion-react-list';
+
+export default {
+  title: 'Examples/Select',
+  component: Select,
+} as Meta;
+
+export const Component: Story = (props: Omit<SelectProps, 'ref'>) => (
+  <div style={{ display: 'flex', justifyContent: 'center', height: '200px' }}>
+    <div style={{ position: 'relative' }}>
+      <Select {...props}>
+        <ListItem>List 1</ListItem>
+        <ListItem>List 2</ListItem>
+        <ListItem>List 3</ListItem>
+      </Select>
+    </div>
+  </div>
+);
