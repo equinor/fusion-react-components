24c3819c15f0da57a81291228f583262d4869199
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2022-04-12 15:02:15 +0200

    fix!: propperty mapping create-element
    when attributes are used  in the `connectCallback` life-cycle of web-components, react has not yeat call back for mapping native attributes.
    
    + extract native data types and provide as props on initial render
    + provide native data types as react props on initial render

--- packages/radio/src/RadioBase.tsx
+++ packages/radio/src/RadioBase.tsx
@@ -1,14 +1,16 @@
 import { createComponent } from '@equinor/fusion-react-utils';
 import { RadioElement as HTMLRadioCustomElement, tag } from '@equinor/fusion-wc-radio';
 
-export type RadioBaseProps = Partial<
-  Pick<HTMLRadioCustomElement, 'checked' | 'disabled' | 'name' | 'reducedTouchTarget' | 'value'>
+type NativeProps = Pick<
+  HTMLRadioCustomElement,
+  'checked' | 'disabled' | 'name' | 'reducedTouchTarget' | 'value' | 'global'
 >;
 
-export const RadioBase = createComponent<HTMLRadioCustomElement, RadioBaseProps>(HTMLRadioCustomElement, tag, {
-  // add checked since this is a get/set
-  functions: new Set(['checked']),
-});
+type NativeRequiredProps = Partial<Pick<NativeProps, 'name'>>;
+
+export type RadioBaseProps = NativeRequiredProps & Partial<NativeProps>;
+
+export const RadioBase = createComponent<HTMLRadioCustomElement, RadioBaseProps>(HTMLRadioCustomElement, tag);
 
 export { HTMLRadioCustomElement };
 
--- packages/utils/src/create-element.ts
+++ packages/utils/src/create-element.ts
@@ -1,4 +1,4 @@
-import { createElement, forwardRef } from 'react';
+import { createElement, forwardRef, useMemo } from 'react';
 import { extractElementProps, useElementEvents, useElementProps, useForwardRef } from './hooks';
 
 type ComponentAttributes<T = HTMLHtmlElement> = Omit<React.HTMLAttributes<T>, 'children'>;
@@ -15,6 +15,9 @@ const translateReactAttribute = (k: string) => {
   }
   return k;
 };
+
+/** @see @link [Data Types](https://www.programiz.com/javascript/data-types) */
+const SUPPORTED_REACT_PROP_TYPES = ['string', 'number', 'boolean', 'bigint'];
 
 /**
  * Wraps a custom element as a React Component
@@ -54,12 +57,33 @@ export const createComponent = <E extends HTMLElement, P extends Record<string, 
     /** bind custom events */
     useElementEvents(ref, props as EventProps, events);
 
-    /** properties which React should handle */
-    const reactProps = Object.entries(props || {})
-      .filter(([k]) => !nativePropsName.has(k))
-      .reduce((c, [k, v]) => Object.assign(c, { [translateReactAttribute(k)]: v }), {});
+    typeof props === 'bigint'
 
-    return createElement(tag, { ...reactProps, ref });
+    /** properties which React should handle */
+    const reactProps = useMemo(() => {
+      const entries = Object.entries(props || {});
+      const reactEntries = entries.filter(([k]) => !nativePropsName.has(k));
+      const nativeEntries = ref.current
+        ? []
+        : entries
+            /**  filter out native property entries */
+            .filter(([k]) => nativePropsName.has(k))
+            /** filter supported properties  */
+            .filter(([_, v]) => SUPPORTED_REACT_PROP_TYPES.includes(typeof v))
+            /** filter out empty properties */
+            .filter(([_, v]) => !!v);
+
+      return (
+        /** combine properties */
+        [...reactEntries, ...nativeEntries]
+          /** translate property name to reactName */
+          .map(([k, v]) => [translateReactAttribute(k), v] as [string, unknown])
+          /** build property object */
+          .reduce((c, [k, v]) => Object.assign(c, { [k]: v }), { ref })
+      );
+    }, [ref, props]);
+
+    return createElement(tag, reactProps);
   });
 
   component.displayName = displayName;
