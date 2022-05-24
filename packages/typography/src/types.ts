f6ea689b9c8d336a89ac9c7040e4c3437e90c916
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2022-05-24 08:45:15 +0200

    fix(typography): component tag type
    - fix creation of React element
    - fix typing of properties
    - fix style classes

--- packages/typography/src/types.ts
+++ packages/typography/src/types.ts
@@ -1,4 +1,6 @@
 import { FusionTheme } from '@equinor/fusion-react-styles';
 
 export type TypographyType = FusionTheme['typography'];
+export type TypographyPropertiesType<K extends keyof TypographyType> = TypographyType[K];
+
 export type TypographyHeadingType = TypographyType['heading'];
