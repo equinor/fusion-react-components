f5dd8ec8e02096cdfea0cb74f6b2a84107ec3ff6
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2022-06-14 14:08:30 +0200

    fix(icon): change typo in prop interface

--- packages/icon/src/index.tsx
+++ packages/icon/src/index.tsx
@@ -1,12 +1,15 @@
+import { PropsWithChildren } from 'react';
+import type { ComponentProps } from 'react';
+
 import { createComponent } from '@equinor/fusion-react-utils';
 import { IconElement as HTMLIconCustomElement, iconNames, tag } from '@equinor/fusion-wc-icon';
 
 export { HTMLIconCustomElement, iconNames };
 
-type ElementProps = React.PropsWithChildren<Partial<Pick<HTMLIconCustomElement, 'icon' | 'type'>>>;
+type ElementProps = PropsWithChildren<Partial<Pick<HTMLIconCustomElement, 'icon' | 'type'>>>;
 
 export const Icon = createComponent<HTMLIconCustomElement, ElementProps>(HTMLIconCustomElement, tag);
 
-export type DateRangeProps = React.ComponentProps<typeof Icon>;
+export type IconProps = ComponentProps<typeof Icon>;
 
 export default Icon;
