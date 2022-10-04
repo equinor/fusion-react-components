3f48090be9128660a7c5ac89103b6429199a575e
Øyvind Eikeland <oyvind@eikeland.me>
2022-10-04 16:09:32 +0200

    feat(select): exporting web component

--- packages/select/src/Select.tsx
+++ packages/select/src/Select.tsx
@@ -1,18 +1,20 @@
 /* eslint-disable react/no-multi-comp */
-import { tag, SelectElement } from '@equinor/fusion-wc-select';
+import { tag, SelectElement as HTMLSelectCustomElement } from '@equinor/fusion-wc-select';
 
 import { ComponentProps, createComponent } from '@equinor/fusion-react-utils';
 
-type ElementAttributes = Partial<
-  Pick<SelectElement, 'value' | 'name' | 'label' | 'icon' | 'disabled' | 'outlined' | 'required'>
+type ElementAttributes = React.PropsWithChildren<
+  Partial<Pick<HTMLSelectCustomElement, 'value' | 'name' | 'label' | 'icon' | 'disabled' | 'outlined' | 'required'>>
 >;
 
 type ElementProps = ElementAttributes;
 
-export const Select = createComponent<SelectElement, ElementProps>(SelectElement, tag, {
+export const Select = createComponent<HTMLSelectCustomElement, ElementProps>(HTMLSelectCustomElement, tag, {
   events: { onSelected: 'selected' },
 });
 
-export type SelectProps = ComponentProps<SelectElement, ElementProps>;
+export type SelectProps = ComponentProps<HTMLSelectCustomElement, ElementProps>;
+
+export { HTMLSelectCustomElement };
 
 export default Select;
