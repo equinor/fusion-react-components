77e4bb8dd5b1ab8a192b3cb39f669c613677496f
Andrej Nikolic <annik@bouvet.no>
2022-10-27 10:21:00 +0200

    chore(person-card): code comment and cleanup

--- packages/person/src/PersonProvider.tsx
+++ packages/person/src/PersonProvider.tsx
@@ -4,10 +4,17 @@ import { PersonAvailability, PersonDetails } from '@equinor/fusion-wc-person';
 
 HTMLPersonProviderCustomElement;
 
+/** Person porvider properties for the resolver */
 export type PersonProviderProps = {
+  /** Person resolver interface that contains getPresence and getDetails functions */
   resolve: PersonResolver;
 };
 
+/**
+ * Person provider component that checks and sets the resolver given as components property
+ * @param props resolver and children
+ * @returns wrapped fusion web-components person provider with its reference around children
+ */
 export const PersonProvider = (props: PropsWithChildren<PersonProviderProps>) => {
   const { resolve } = props;
   const ref = useRef<HTMLPersonProviderCustomElement>(null);
@@ -24,6 +31,11 @@ export const PersonProvider = (props: PropsWithChildren<PersonProviderProps>) =>
   return <fwc-person-provider ref={ref}>{props.children}</fwc-person-provider>;
 };
 
+/**
+ * @param details Person details as an object property or a single value
+ * @param presence Person presence as an object property or a single value
+ * @returns Checks if details and/or presence type and creates a resolver
+ */
 export const createPersonResolver = (
   details: PersonResolver['getDetails'] | PersonDetails,
   presence?: PersonResolver['getPresence'] | PersonAvailability
