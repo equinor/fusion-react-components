24ae5f4e7ff6468f9a046a9e3d5ea955a2258c1d
Øyvind Eikeland <oyvind@eikeland.me>
2022-11-24 16:45:11 +0100

    fix: updated contextselector and sdd to match webcomponent and storybook

--- storybook/src/stories/searchable-dropdown/component.helpers.tsx
+++ storybook/src/stories/searchable-dropdown/component.helpers.tsx
@@ -1,7 +1,7 @@
-import { SearchableDropdownResultItem, SearchableDropdownResult } from '@equinor/fusion-wc-searchable-dropdown';
+import { SearchableDropdownResultItem, SearchableDropdownResult } from '@equinor/fusion-react-searchable-dropdown';
 
 /* generates a single SearchableDropdownResult item with required propterties  */
-const singleItem = (props: unknown): SearchableDropdownResultItem => {
+const singleItem = (props: Partial<SearchableDropdownResultItem>): SearchableDropdownResultItem => {
   return Object.assign({ id: '0', title: 'Dummy title' }, props);
 };
 
@@ -11,60 +11,78 @@ const singleItem = (props: unknown): SearchableDropdownResultItem => {
  * Query string min length to start "http request" is 3 chars.
  * uses singleItem() to create a single result with errors or other information
  */
-const fakeApi = (query: string): SearchableDropdownResult => {
-  /* min length */
-  const min = 3;
-  const items = [];
-  if (!query || query.length < min) {
-    items.push(singleItem({ title: `Add ${min - query.length} chars to search`, isDisabled: true }));
-    return items;
-  }
-  const allResults = [
-    {
-      id: '0001',
-      title: 'Johan Castberg',
-      isError: true,
-      meta: 'error_outlined',
-    },
-    {
-      id: '0002',
-      title: 'Johan Sverdrup Business Case',
-      subTitle: 'some project description...',
-      isSelected: true,
-      meta: 'check',
-    },
-    {
-      id: '0003',
-      title: 'Johan Sverdrup Phase 2',
-    },
-    {
-      id: '0004',
-      title: 'Johan Castberg Prosjektportal',
-    },
-  ];
+const allItems = [
+	{"id":"section-001","title":"Apps prod","type":"section","children":[
+		{"id":"8aa0d62f-21d4-4933-a1e1-823a8de7c13c","title":"Johan prod","subTitle":"An App from Fusion","graphic":"settings"},
+		{"id":"8aa0d62f-21d4-4933-a1e1-823a8de7c13d","title":"Johan2 prod","subTitle":"An App from Fusion","graphic":"settings"},
+		{"id":"8aa0d62f-21d4-4933-a1e1-823a8de7c13e","title":"Johan3 prod","subTitle":"An App from Fusion","graphic":"settings"},
+		{"id":"8aa0d62f-21d4-4933-a1e1-823a8de7c13f","title":"Johan4 prod","subTitle":"An App from Fusion","graphic":"settings"}]
+	},
+	{"id":"section-002","title":"Projects prod","type":"section","children":[
+		{"id":"8aa0d62f-21d4-4933-a1e1-823a8de7c14a","title":"Project1 prod","subTitle":"A Fusion context","graphic":"settings"},
+		{"id":"8aa0d62f-21d4-4933-a1e1-823a8de7c14b","title":"Project2 prod","subTitle":"A Fusion context","graphic":"settings"},
+		{"id":"8aa0d62f-21d4-4933-a1e1-823a8de7c14c","title":"Project3 prod","subTitle":"A Fusion context","graphic":"settings"},
+		{"id":"8aa0d62f-21d4-4933-a1e1-823a8de7c14d","title":"Project4 prod","subTitle":"A Fusion context","graphic":"settings"}]
+	},
+	{"id":"8aa0d62f-21d4-4933-a1e1-823a8de7c15a","title":"Unsectioned prod","subTitle":"Some random item"},
+	{"id":"8aa0d62f-21d4-4933-a1e1-823a8de7c15b","title":"Unsectioned2 prod","subTitle":"Some random item"},
+	{"id":"8aa0d62f-21d4-4933-a1e1-823a8de7c15c","title":"Unsectioned3 prod","subTitle":"Some random item"},
+	{"id":"8aa0d62f-21d4-4933-a1e1-823a8de7c15d","title":"Unsectioned4 prod","subTitle":"Some random item"}
+];
 
-  for (const item of allResults) {
-    if (item.title.toLowerCase().indexOf(query) > -1) {
-      items.push(item);
+/* Dummy api handler */
+const apiItems = (query: string): SearchableDropdownResult => {
+  /* min length of query string */
+  const min = 2;
+  const matched = [];
+  if (!query || query.length < min) {
+    matched.push(singleItem({ title: `Need ${min - query.length} more chars`, isDisabled: true }));
+    return matched;
+  }
+
+  /* Recursive func for matching in children  */
+  for (const item of allItems as SearchableDropdownResult) {
+    const entry = { ...item };
+    // Match against children in sections
+    if (entry.type === 'section' && entry.children?.length) {
+      entry.children = entry.children.filter((i) => i.title && i.title.toLowerCase().indexOf(query) > -1);
+      if (entry.children.length) {
+        matched.push(entry);
+      }
+    } else if (entry.title && entry.title.toLowerCase().indexOf(query) > -1) {
+      matched.push(entry);
     }
   }
 
-  if (!items.length) {
-    items.push(singleItem({ title: 'No matches...', isDisabled: true }));
+  if (!matched.length) {
+    matched.push(singleItem({ title: 'No matches...', isDisabled: true }));
   }
 
-  return items;
+  return matched;
 };
 
 /* Example resolver for lit controller task */
 export const _exampleResolver = {
   searchQuery: async (query: string) => {
     try {
-      return fakeApi(query);
+      return apiItems(query);
     } catch (e) {
-      return [singleItem({ item: 'API Error', subTitle: e, isDisabled: true, isError: true })];
+      return [singleItem({ title: 'API Error', isDisabled: true, isError: true })];
     }
   },
+  initialResult: [
+    singleItem({
+      id: '123',
+      title: 'Initial Items',
+      type: 'section',
+      children: [
+        singleItem({ id: '456', title: 'Initial Item 1', graphic: 'list' }),
+        singleItem({ id: '654', title: 'Initial Item 2', graphic: 'list' }),
+        singleItem({ id: '789', title: 'Initial Item 3', graphic: 'list' }),
+        singleItem({ id: '321', title: 'Initial Item 4', graphic: 'list' }),
+      ],
+    }),
+  ],
 };
 
 /* Listen to action event */
