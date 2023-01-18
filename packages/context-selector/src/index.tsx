f40e5e31e124551d621d0599b1cda6de6abda791
Øyvind Eikeland <oyvind@eikeland.me>
2023-01-18 16:22:32 +0100

    fix(ContextSearch): export ContextSearch from index

--- packages/context-selector/src/index.tsx
+++ packages/context-selector/src/index.tsx
@@ -1,6 +1,6 @@
 export * from './ContextProvider';
 export * from './ContextSelector';
-export * from './ContextSelectorHeader';
+export * from './ContextSearch';
 export * from './types';
 
 export { ContextSelector as default } from './ContextSelector';
