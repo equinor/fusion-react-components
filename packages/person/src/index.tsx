b4c6e057885cef56029dc5537eaa96552637e064
Andrej Nikolic <andrej.nikolic@bouvet.no>
2023-01-30 07:58:33 +0100

    fix: :bug: additional type export

--- packages/person/src/index.tsx
+++ packages/person/src/index.tsx
@@ -1,7 +1,5 @@
 export * from './PersonAvatar';
 export * from './PersonCard';
 export * from './PersonProvider';
-
 export type { PersonResolver } from '@equinor/fusion-wc-person/person-provider';
-
-export { PersonAvailability, PersonAccountType } from '@equinor/fusion-wc-person';
+export * from '@equinor/fusion-wc-person';
