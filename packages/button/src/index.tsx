4acf8562c7b5b7a528ed9818527beb3674a8f370
Andrej Nikolic <andrej.nikolic@bouvet.no>
2023-02-03 15:09:19 +0100

    feat: :sparkles: icon button toggle
    added web component icon button toggle as react component

--- packages/button/src/index.tsx
+++ packages/button/src/index.tsx
@@ -1,3 +1,4 @@
 export { LinkButton, LinkButtonProps, HTMLLinkButtonCustomElement } from './link-button';
 export { Button, ButtonProps, HTMLButtonCustomElement } from './button';
 export { IconButton, IconButtonColor, HTMLIconButtonCustomElement } from './icon-button';
+export { IconButtonToggle, IconButtonToggleProps, HTMLIconButtonToggleCustomElement } from './icon-button-toggle';
