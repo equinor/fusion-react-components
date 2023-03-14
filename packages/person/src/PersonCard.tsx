f1bc43daa05ed56ceca7d063b5cc3118ed8d61bc
Andrej Nikolic <andrej.nikolic@bouvet.no>
2023-03-14 08:08:30 +0100

    fix(person-card): person item size

--- packages/person/src/PersonCard.tsx
+++ packages/person/src/PersonCard.tsx
@@ -1,11 +1,12 @@
 import { PropsWithChildren } from 'react';
 import { ComponentProps, createComponent } from '@equinor/fusion-react-utils';
-import HTMLPersonCardCustomElement, { tag, AvatarSize } from '@equinor/fusion-wc-person/person-card';
+import HTMLPersonCardCustomElement, { tag } from '@equinor/fusion-wc-person/person-card';
+import { PersonItemSize } from '@equinor/fusion-wc-person';
 
 type ElementProps = PropsWithChildren<Partial<Pick<HTMLPersonCardCustomElement, 'azureId' | 'size'>>>;
 
 export type PersonCardProps = ComponentProps<HTMLPersonCardCustomElement, ElementProps>;
 export const PersonCard = createComponent<HTMLPersonCardCustomElement, ElementProps>(HTMLPersonCardCustomElement, tag);
 
-export { HTMLPersonCardCustomElement, AvatarSize };
+export { HTMLPersonCardCustomElement, PersonItemSize };
 export default PersonCard;
