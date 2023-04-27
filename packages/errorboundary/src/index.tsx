a8561716423b4f8e8134d01981729ac2db6f8aa5
Øyvind Eikeland <oyvind@eikeland.me>
2023-04-27 12:32:26 +0200

    fix(errorboundary): errormessage

--- packages/errorboundary/src/index.tsx
+++ packages/errorboundary/src/index.tsx
@@ -1,32 +1,21 @@
 import { Component, PropsWithChildren } from 'react';
 
 import ErrorMessage from './ErrorMessage';
-import useErrorStyles from './useErrorStyles';
 import type { ErrorMessageProps } from './types';
 
-const styles = useErrorStyles();
-
 export class ErrorBoundary extends Component<PropsWithChildren<ErrorMessageProps>> {
-  state = { didCatch: false };
+  state = { hasError: false };
 
   componentDidCatch() {
-    this.setState({ didCatch: true });
+    this.setState({ hasError: true });
   }
 
   render() {
-    if (this.state.didCatch) {
-      return (
-        <div className={styles.container}>
-          <div className={styles.messageContainer}>
-            <div className={styles.title}>Something went wrong</div>
-            <div className={styles.message}>
-              Please try refresh browser, if problem persists, please contact support through service now
-            </div>
-          </div>
-        </div>
-      );
-    }
-    return <ErrorMessage {...this.props}>{this.props.children}</ErrorMessage>;
+    return (
+      <ErrorMessage hasError={this.state.hasError} {...this.props}>
+        {this.props.children}
+      </ErrorMessage>
+    );
   }
 }
 
