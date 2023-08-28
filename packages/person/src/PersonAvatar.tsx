126f02dc91c12a289ca772be13e256fe4a117d3f
Andrej Nikolic <andrej.nikolic@bouvet.no>
2023-08-28 10:55:51 +0200

    feat(person-avatar): person avatar component rework

--- packages/person/src/PersonAvatar.tsx
+++ packages/person/src/PersonAvatar.tsx
@@ -1,16 +1,34 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useEffect, useRef } from 'react';
 import { ComponentProps, createComponent } from '@equinor/fusion-react-utils';
 import HTMLPersonAvatarCustomElement, { tag, AvatarSize } from '@equinor/fusion-wc-person/person-avatar';
+import extractProps from './extract-props';
 
 type ElementProps = PropsWithChildren<
-  Partial<Pick<HTMLPersonAvatarCustomElement, 'azureId' | 'size' | 'clickable' | 'disabled'>>
+  Partial<Pick<HTMLPersonAvatarCustomElement, 'azureId' | 'upn' | 'dataSource' | 'size' | 'clickable' | 'disabled'>>
 >;
 
 export type PersonAvatarProps = ComponentProps<HTMLPersonAvatarCustomElement, ElementProps>;
-export const PersonAvatar = createComponent<HTMLPersonAvatarCustomElement, ElementProps>(
+
+const PersonAvatarComponent = createComponent<HTMLPersonAvatarCustomElement, ElementProps>(
   HTMLPersonAvatarCustomElement,
   tag
 );
 
-export { HTMLPersonAvatarCustomElement, AvatarSize };
+export const PersonAvatar = ({ children, ...props }: PropsWithChildren<PersonAvatarProps>): JSX.Element => {
+  const avatarRef = useRef<HTMLPersonAvatarCustomElement>(null);
+
+  useEffect(() => {
+    for (const [name, value] of Object.entries(extractProps<ElementProps>(props))) {
+      if (avatarRef.current) {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        avatarRef.current[name] = value;
+      }
+    }
+  }, []);
+
+  return <PersonAvatarComponent ref={avatarRef}>{children}</PersonAvatarComponent>;
+};
+
+export { AvatarSize };
 export default PersonAvatar;
