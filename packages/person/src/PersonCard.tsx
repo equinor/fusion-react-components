c139cc471689362d890534d1ba8fc1f00b948f4c
Andrej Nikolic <andrej.nikolic@bouvet.no>
2023-09-07 14:44:12 +0200

    feat(person-component): person card and person list item upgrade

--- packages/person/src/PersonCard.tsx
+++ packages/person/src/PersonCard.tsx
@@ -1,14 +1,31 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useEffect, useRef } from 'react';
 import { ComponentProps, createComponent } from '@equinor/fusion-react-utils';
-import HTMLPersonCardCustomElement, { tag } from '@equinor/fusion-wc-person/person-card';
+import HTMLPersonCardCustomElement, { tag, CardData } from '@equinor/fusion-wc-person/person-card';
 import { PersonItemSize } from '@equinor/fusion-wc-person';
+import extractProps from './extract-props';
 
 type ElementProps = PropsWithChildren<
-  Partial<Pick<HTMLPersonCardCustomElement, 'azureId' | 'size' | 'maxWidth' | 'contentHeight'>>
+  Partial<Pick<HTMLPersonCardCustomElement, 'azureId' | 'upn' | 'dataSource' | 'size' | 'maxWidth' | 'contentHeight'>>
 >;
 
 export type PersonCardProps = ComponentProps<HTMLPersonCardCustomElement, ElementProps>;
-export const PersonCard = createComponent<HTMLPersonCardCustomElement, ElementProps>(HTMLPersonCardCustomElement, tag);
+export const PersonCardComponent = createComponent<HTMLPersonCardCustomElement, ElementProps>(HTMLPersonCardCustomElement, tag);
 
-export { HTMLPersonCardCustomElement, PersonItemSize };
+export const PersonCard = ({ children, ...props }: PropsWithChildren<PersonCardProps>): JSX.Element => {
+  const cardRef = useRef<HTMLPersonCardCustomElement>(null);
+
+  useEffect(() => {
+    for (const [name, value] of Object.entries(extractProps<ElementProps>(props))) {
+      if (cardRef.current) {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        cardRef.current[name] = value;
+      }
+    }
+  }, []);
+
+  return <PersonCardComponent ref={cardRef}>{children}</PersonCardComponent>;
+};
+
+export { HTMLPersonCardCustomElement, PersonItemSize, CardData };
 export default PersonCard;
