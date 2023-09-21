ef85499903c09bb57c8b99e66af2b3125f2e0569
Andrej Nikolic <andrej.nikolic@bouvet.no>
2023-09-21 09:53:05 +0200

    fix(react-components): lint fixes

--- packages/filter/src/components/filter/checkbox/CheckboxFilterProvider.tsx
+++ packages/filter/src/components/filter/checkbox/CheckboxFilterProvider.tsx
@@ -9,9 +9,9 @@ import type { FilterFn } from '../../../types';
 
 const createFilterFn =
   <TData extends Record<string, unknown>, TValue = string>(selector: FilterOptionSelector<TData>) =>
-    (data: TData[], selection: Set<TValue>) => {
-      return selection.size ? data.filter((x) => selection.has(selector(x).key as unknown as TValue)) : data;
-    };
+  (data: TData[], selection: Set<TValue>) => {
+    return selection.size ? data.filter((x) => selection.has(selector(x).key as unknown as TValue)) : data;
+  };
 
 export type CheckboxFilterProviderProps<
   TData extends Record<string, any>,
@@ -35,28 +35,28 @@ export const CheckboxFilterProvider: <
 ) => JSX.Element = <TData extends Record<string, any>, TOptions extends CheckboxOption = CheckboxOption>(
   props: React.PropsWithChildren<CheckboxFilterProviderProps<TData, TOptions>>,
 ): JSX.Element => {
-    const { filterKey, selector = filterKey, title, initial, filter, children } = props;
-    const selectorFn = useMemo(
-      () =>
-        typeof selector === 'function'
-          ? selector
-          : propertySelector(selector || (selector as Extract<keyof TData, string>)),
-      [selector],
-    );
-    const filterFn = useMemo<FilterFn<TData, Set<string>>>(() => {
-      if (filter?.filterFn) return filter.filterFn;
-      if (!selectorFn) throw Error('Could not create filter. Please provide either filterFn or filterSelector.');
-      return createFilterFn(selectorFn);
-    }, [selectorFn, filter?.filterFn]);
-    return (
-      <FilterOptionProvider
-        selector={selectorFn}
-        optionBuilder={filter?.optionFn}
-        filter={{ key: filterKey, title, initial, filterFn }}
-      >
-        {children}
-      </FilterOptionProvider>
-    );
-  };
+  const { filterKey, selector = filterKey, title, initial, filter, children } = props;
+  const selectorFn = useMemo(
+    () =>
+      typeof selector === 'function'
+        ? selector
+        : propertySelector(selector || (selector as Extract<keyof TData, string>)),
+    [selector],
+  );
+  const filterFn = useMemo<FilterFn<TData, Set<string>>>(() => {
+    if (filter?.filterFn) return filter.filterFn;
+    if (!selectorFn) throw Error('Could not create filter. Please provide either filterFn or filterSelector.');
+    return createFilterFn(selectorFn);
+  }, [selectorFn, filter?.filterFn]);
+  return (
+    <FilterOptionProvider
+      selector={selectorFn}
+      optionBuilder={filter?.optionFn}
+      filter={{ key: filterKey, title, initial, filterFn }}
+    >
+      {children}
+    </FilterOptionProvider>
+  );
+};
 
 export default CheckboxFilterProviderProps;
