69add6ea0f5dd2da4bff1459b719e6c4c885cd39
Andrej Nikolic <andrej.nikolic@bouvet.no>
2023-09-25 09:53:45 +0200

    fix(person-components): export

--- packages/person/src/index.tsx
+++ packages/person/src/index.tsx
@@ -1,10 +1,18 @@
-export * from './PersonAvatar';
-export * from './PersonCard';
-export * from './PersonListItem';
-export * from './PersonSelect';
-export * from './Provider';
-export type { PersonResolver } from '@equinor/fusion-wc-person/provider';
+// export components
+export { PersonAvatar } from './PersonAvatar';
+export { PersonCard } from './PersonCard';
+export { PersonListItem } from './PersonListItem';
+export { PersonSelect } from './PersonSelect';
+export { Provider } from './Provider';
+// export enum
+export { AvatarSize } from './PersonAvatar';
 export { PersonAvailability, PersonAccountType } from '@equinor/fusion-wc-person';
+// export types
+export type { AvatarData, PersonAvatarProps } from './PersonAvatar';
+export type { CardData, PersonItemSize, PersonCardProps } from './PersonCard';
+export type { ListItemData, PersonListItemProps } from './PersonListItem';
+export type { PersonSelectProps } from './PersonSelect';
+export type { PersonResolver } from '@equinor/fusion-wc-person/provider';
 export type {
   PersonDetails,
   PersonPresence,
