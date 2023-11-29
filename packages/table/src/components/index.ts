412da2a5f6cfd817e9ab6f5b30e9b9343423b2c7
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2023-11-29 13:26:11 +0100

    chore(table): remove package (#1217)

--- packages/table/src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './ColumnDefault';
-export * from './ColumnHeader';
-export * from './menu';
-export * from './filters';
-export * from './icons';
-export * from './toolbar';
--- packages/table/src/components/menu/index.ts
+++
@@ -1,6 +0,0 @@
-import * as ColumnMenuItems from './items';
-
-export { ColumnMenuItems };
-
-export * from './ColumnMenu';
-export * from './ColumnMenuButton';
