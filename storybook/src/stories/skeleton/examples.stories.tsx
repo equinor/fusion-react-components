ff0440f89dee53db66d83b9ee1160732a9510334
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2024-01-11 12:31:32 +0100

    fix(storybook): fix skeleton stories
    Closes: AB#47910

--- storybook/src/stories/skeleton/examples.stories.tsx
+++ storybook/src/stories/skeleton/examples.stories.tsx
@@ -1,38 +1,39 @@
 import { Meta, Story } from '@storybook/react';
-import { Skeleton, SkeletonWrapper, SkeletonProps, SkeletonVariant } from '@equinor/fusion-react-skeleton/src';
-import { iconNames } from '@equinor/fusion-react-icon/src';
+import { Skeleton, SkeletonProps, SkeletonSize, SkeletonVariant } from '@equinor/fusion-react-skeleton/src';
 
 export default {
   title: 'Examples/Skeleton',
   component: Skeleton,
-  argTypes: {
-    icon: {
-      control: 'select',
-      options: iconNames,
-    },
-  },
 } as Meta;
 
-type ComponentProps = SkeletonProps & { initial: string };
-export const Component: Story<ComponentProps> = (props: ComponentProps) => <Skeleton {...props} />;
+export const Component: Story<SkeletonProps> = (props: SkeletonProps) => (
+  <Skeleton size={SkeletonSize.Medium} {...props} />
+);
 
 export const Default: Story = () => <Skeleton></Skeleton>;
-
-export const Square: Story = () => <Skeleton variant={SkeletonVariant.Square}></Skeleton>;
-
-export const Circle: Story = () => <Skeleton variant={SkeletonVariant.Circle}></Skeleton>;
-
-export const Text: Story = () => <Skeleton variant={SkeletonVariant.Text}></Skeleton>;
-
-export const Fluid: Story = () => <Skeleton variant={SkeletonVariant.Text} fluid></Skeleton>;
-
-export const Combination: Story = () => (
-  <SkeletonWrapper>
-    <Skeleton variant={SkeletonVariant.Square} icon="image" />
-    <SkeletonWrapper direction="vertical">
-      <Skeleton size="large" variant={SkeletonVariant.Text} />
-      <Skeleton size="medium" variant={SkeletonVariant.Text} />
-      <Skeleton size="large" variant={SkeletonVariant.Text} />
-    </SkeletonWrapper>
-  </SkeletonWrapper>
+export const WithText: Story = () => <Skeleton>Fusion</Skeleton>;
+export const WithCustomColor: Story = () => (
+  <Skeleton
+    style={{ '--fwc-skeleton-fill-color': 'rgba(0, 111, 0, 0.25)', '--fwc-skeleton-ink-color': '#000' }}
+    variant={SkeletonVariant.Text}
+  >
+    Custom Color
+  </Skeleton>
 );
+export const Sizes: Story = () =>
+  Object.values(SkeletonSize).map((size) => (
+    <>
+      <Component key={size} size={size}></Component>
+      <br />
+    </>
+  ));
+
+export const Variants: Story = () =>
+  Object.values(SkeletonVariant).map((variant) => (
+    <div key={variant} style={{ display: 'flex' }}>
+      <div>
+        <p>{variant}</p>
+        <Component variant={variant}></Component>
+      </div>
+    </div>
+  ));
