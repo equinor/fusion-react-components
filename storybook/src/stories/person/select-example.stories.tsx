89a00d2f296b73f40bfe20042f48784d93338272
Øyvind Eikeland <oyvind@eikeland.me>
2024-02-15 16:15:40 +0100

    docs(storybook): story for PersonSelect selectedPerson attribute

--- storybook/src/stories/person/select-example.stories.tsx
+++ storybook/src/stories/person/select-example.stories.tsx
@@ -1,22 +1,15 @@
 import { Meta, Story } from '@storybook/react';
-import { PersonProvider, PersonAccountType, PersonSelect, PersonSelectProps } from '@equinor/fusion-react-person/src';
+import { PersonProvider, PersonSelect, PersonSelectProps } from '@equinor/fusion-react-person/src';
 import { createResolve } from './resolve-mock/person-resolve-mock';
+import { PersonSelectEvent } from '../../../../packages/person/src/PersonSelect';
 
 export default {
   title: 'Examples/Person/Person Select',
   component: PersonSelect,
   argTypes: {
-    id: {
-      type: 'string',
-      description: 'HTML ID for component',
-    },
     placeholder: {
       type: 'string',
       description: 'TextInput placeholder string',
-    },
-    initialText: {
-      type: 'string',
-      description: 'First item in dropdown before any search is done',
     },
     variant: {
       type: 'string',
@@ -33,50 +26,30 @@ export default {
     value: {
       description: 'The selected value do display in TextInput',
     },
-    meta: {
-      description: 'A fwc-icon name to show AFTER each list item. may also be set per result item.',
-    },
-    graphic: {
-      description: 'A fwc-icon name to show BEFORE each list item. may also be set per result item.',
-    },
     autofocus: {
       type: 'boolean',
       description: 'Flag for setting focus on textInput on component mount',
-    },
-    multiple: {
-      description: 'Flag for selecting multiple items.',
     },
     disabled: {
       type: 'boolean',
       description: 'Disable text input',
     },
-    selectedId: {
-      type: 'string',
-      description: 'The pre-selected id to higlight',
-    },
     leadingIcon: {
       description: 'Leading Icon to display in text input',
     },
   },
+  args: {
+    placeholder: 'Start to type to search...',
+    dropdownHeight: '300px',
+    onSelect: (w: PersonSelectEvent) => console.log('onSelect', w),
+    onDropdownClosed: (w: Event) => console.log('onDropdownClosed', w),
+  },
 } as Meta;
 
-export type CardProps = PersonSelectProps & {
-  accountType: PersonAccountType;
-};
-
-export const Component: Story<CardProps> = (props: CardProps) => (
+export const Component: Story<PersonSelectProps> = (props: PersonSelectProps) => (
   <PersonProvider resolve={createResolve}>
     <div style={{ maxWidth: '420px', height: '350px', margin: '3em auto' }}>
       <PersonSelect {...props} />
     </div>
   </PersonProvider>
 );
-Component.args = {
-  placeholder: 'Start to type to search...',
-  initialText: 'The initial text result',
-  variant: 'page',
-  dropdownHeight: '300px',
-  leadingIcon: 'search',
-  onSelect: (w) => console.log('onSelect', w),
-  onDropdownClosed: (w) => console.log('onDropdownClosed', w),
-};
