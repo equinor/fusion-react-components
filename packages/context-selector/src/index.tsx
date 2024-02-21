730529e78426c2ae012d6842acdc5934598ecfcd
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2024-02-21 08:35:54 +0100

    style(context-selector): refactor exports in Context Selector and update types

--- packages/context-selector/src/index.tsx
+++ packages/context-selector/src/index.tsx
@@ -1,6 +1,13 @@
-export * from './ContextProvider';
-export * from './ContextSelector';
-export * from './ContextSearch';
-export * from './types';
+export { ContextProvider } from './ContextProvider';
+export { ContextSelector } from './ContextSelector';
+export { ContextSearch, type ContextSearchProps } from './ContextSearch';
+export type {
+  ContextProviderProps,
+  ContextResolver,
+  ContextResult,
+  ContextResultItem,
+  ContextSelectEvent,
+  ContextSelectorProps,
+} from './types';
 
 export { ContextSelector as default } from './ContextSelector';
