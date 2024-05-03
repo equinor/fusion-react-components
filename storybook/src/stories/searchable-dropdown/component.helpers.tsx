24c0c167c972b9796444cbf997349c483480eb15
Øyvind Eikeland <43904393+eikeland@users.noreply.github.com>
2024-05-03 10:28:04 +0200

    chore(searchable-dropdown): faker data on results in storybook (#1758)
    * chore(searchable-dropdown): faker data on results in storybook
    
    * docs: changeset
    
    * docs(searchabledropdown): story with errors in query result
    
    * docs(searchable-dropdown): commnet to explain no match result on 4th attempt

--- storybook/src/stories/searchable-dropdown/component.helpers.tsx
+++ storybook/src/stories/searchable-dropdown/component.helpers.tsx
@@ -2,170 +2,109 @@ import {
   SearchableDropdownResultItem,
   SearchableDropdownResult,
   SearchableDropdownResolver,
-  IconType,
 } from '@equinor/fusion-react-searchable-dropdown';
 import { eqOneIcon, orgIcon, reviewIcon } from './component.icons';
 
-/* generates a single SearchableDropdownResult item with required propterties  */
-const singleItem = (props: Partial<SearchableDropdownResultItem>): SearchableDropdownResultItem => {
-  return Object.assign({ id: '0', title: 'Dummy title' }, props);
+import { faker } from '@faker-js/faker';
+faker.seed(321456987);
+
+/**
+ * Faker data generator for SearchableDropdownResultItem.
+ */
+const getItems = (len: number, merge?: Partial<SearchableDropdownResultItem>) => {
+  return new Array(len).fill(undefined).map((_) => {
+    return Object.assign(
+      {
+        id: faker.string.uuid(),
+        title: faker.company.name(),
+        subTitle: faker.company.catchPhrase(),
+      },
+      merge ?? {},
+    );
+  });
+};
+
+/**
+ * Faker data generator for section with children.
+ */
+const getSections = (len: number, kids: number) => {
+  return new Array(len).fill(undefined).map((_, i) => {
+    const opts: Partial<SearchableDropdownResultItem> = { graphic: 'list' };
+    if ((i + 1) % 2 === 0) {
+      opts.graphic = orgIcon;
+      opts.graphicType = 'inline-svg';
+    } else if ((i + 1) % 3 === 0) {
+      opts.graphic = eqOneIcon;
+      opts.graphicType = 'inline-svg';
+    } else if ((i + 1) % 4 === 0) {
+      opts.graphic = reviewIcon;
+      opts.graphicType = 'inline-svg';
+    }
+
+    return {
+      id: faker.string.uuid(),
+      title: `Section ${faker.company.buzzVerb()}`,
+      type: 'section',
+      children: getItems(kids, opts),
+    } satisfies SearchableDropdownResultItem;
+  });
 };
 
 /**
  * Example api handler
  * Takes the query string to search for and return matching example projects.
- * Query string min length to start "http request" is 3 chars.
- * uses singleItem() to create a single result with errors or other information
+ * Query string min length to start "http request" is 2 chars.
  */
-
-const allItems = [
-  {
-    id: 'section-001',
-    title: 'Apps prod',
-    type: 'section',
-    children: [
-      {
-        id: '8aa0d62f-21d4-4933-a1e1-823a8de7c13c',
-        title: 'Johan prod',
-        subTitle: 'An App from Fusion 2211',
-        graphic: eqOneIcon,
-        graphicType: 'inline-svg',
-        isDisabled: true,
-      },
-      {
-        id: '8aa0d62f-21d4-4933-a1e1-823a8de7c13d',
-        title: 'Johan2 prod',
-        subTitle: 'An App from Fusion',
-        graphic: 'settings',
-      },
-      {
-        id: '8aa0d62f-21d4-4933-a1e1-823a8de7c13e',
-        title: 'Johan3 prod',
-        subTitle: 'An App from Fusion',
-        graphic: 'settings',
-      },
-      {
-        id: '8aa0d62f-21d4-4933-a1e1-823a8de7c13f',
-        title: 'Johan4 prod',
-        subTitle: 'An App from Fusion',
-        graphic: 'settings',
-      },
-    ],
-  },
-  {
-    id: 'section-002',
-    title: 'Projects prod',
-    type: 'section',
-    children: [
-      {
-        id: '8aa0d62f-21d4-4933-a1e1-823a8de7c14a',
-        title: 'Project1 prod',
-        subTitle: 'A Fusion context',
-        graphic: 'settings',
-      },
-      {
-        id: '8aa0d62f-21d4-4933-a1e1-823a8de7c14b',
-        title: 'Project2 prod',
-        subTitle: 'A Fusion context',
-        graphic: 'settings',
-        isError: true,
-      },
-      {
-        id: '8aa0d62f-21d4-4933-a1e1-823a8de7c14c',
-        title: 'Project3 prod',
-        subTitle: 'A Fusion context',
-        graphic: 'settings',
-      },
-      {
-        id: '8aa0d62f-21d4-4933-a1e1-823a8de7c14d',
-        title: 'Project4 prod',
-        subTitle: 'A Fusion context',
-        graphic: 'settings',
-      },
-    ],
-  },
-  { id: '8aa0d62f-21d4-4933-a1e1-823a8de7c15a', title: 'Unsectioned prod', subTitle: 'Some random item' },
-  { id: '8aa0d62f-21d4-4933-a1e1-823a8de7c15b', title: 'Unsectioned2 prod', subTitle: 'Some random item' },
-  { id: '8aa0d62f-21d4-4933-a1e1-823a8de7c15c', title: 'Unsectioned3 prod', subTitle: 'Some random item' },
-  { id: '8aa0d62f-21d4-4933-a1e1-823a8de7c15d', title: 'Unsectioned4 prod', subTitle: 'Some random item' },
-];
-
-/* Dummy api handler */
-const apiItems = (query: string): SearchableDropdownResult => {
+/* counter to show no match for example */
+let attempts = 0;
+const apiItems = (query: string): SearchableDropdownResult | Error => {
+  attempts++;
   /* min length of query string */
-  const min = 2;
-  const matched = [];
+  const min = 3;
   if (!query || query.length < min) {
-    matched.push(singleItem({ title: `Need ${min - query.length} more chars`, isDisabled: true }));
-    return matched;
+    throw new Error(`Need ${min - query.length} more chars`, { cause: 'length' });
   }
 
-  /* Recursive func for matching in children  */
-  for (const item of allItems as SearchableDropdownResult) {
-    const entry = { ...item };
-    // Match against children in sections
-    if (entry.type === 'section' && entry.children?.length) {
-      entry.children = entry.children.filter((i) => i.title && i.title.toLowerCase().indexOf(query) > -1);
-      if (entry.children.length) {
-        matched.push(entry);
-      }
-    } else if (entry.title && entry.title.toLowerCase().indexOf(query) > -1) {
-      matched.push(entry);
-    }
+  /* Since faker always return a result show "no match" on every 4th call to the resolver */
+  if (attempts % 4 === 0) {
+    throw new Error('No matches found', { cause: 'nomatch' });
   }
 
-  if (!matched.length) {
-    matched.push(singleItem({ title: 'No matches...', isDisabled: true }));
-  }
-
-  return matched;
+  /* return faker result with sections */
+  return getSections(3, 5);
 };
 
-/* Example resolver for lit controller task */
+/* Example resolver for controller task */
 export const _exampleResolver: SearchableDropdownResolver = {
-  searchQuery: async (query: string) => {
+  searchQuery: (query: string) => {
+    let items: SearchableDropdownResult = [];
     try {
-      return apiItems(query);
+      const apiResult = apiItems(query);
+      if (!(apiResult instanceof Error)) {
+        items = apiResult;
+      }
     } catch (e) {
-      return [singleItem({ title: 'API Error', isDisabled: true, isError: true })];
+      const err = e as Error;
+      const opts: Partial<SearchableDropdownResultItem> = {};
+      switch (err.cause) {
+        case 'nomatch':
+          opts.subTitle = `matching: ${query}`;
+          opts.graphic = 'close';
+          break;
+        case 'length':
+          opts.graphic = 'edit';
+          break;
+        default:
+          opts.isError = true;
+      }
+
+      // error item
+      items = [Object.assign({ id: faker.string.uuid(), title: err.message, isDisabled: true }, opts)];
     }
+
+    return items;
   },
-  initialResult: [
-    singleItem({
-      id: 'init-123',
-      title: 'Initial Items',
-      type: 'section',
-      children: [
-        singleItem({
-          id: '456',
-          title: 'Initial Item 1',
-          graphic: orgIcon,
-          graphicType: 'inline-svg',
-          meta: 'Meta content',
-        }),
-        singleItem({
-          id: '654',
-          title: 'Initial Item 2',
-          graphicType: 'inline-svg',
-          graphic: reviewIcon,
-        }),
-        singleItem({
-          id: '789',
-          title: 'Initial Item 3',
-          graphic: 'list',
-        }),
-        singleItem({ id: '321', title: 'Initial Item 4', graphic: 'list' }),
-        singleItem({ id: '012', title: 'Initial Item 5', graphic: 'add', graphicType: IconType.EDS, isDisabled: true }),
-        singleItem({
-          id: '123',
-          title: 'Initial Item 6',
-          graphic: 'close_circle_outlined',
-          graphicType: IconType.EDS,
-          isError: true,
-        }),
-      ],
-    }),
-  ],
+  initialResult: getSections(1, 10),
   closeHandler: (e: Event) => {
     console.log('UI closed dropdown list.', e);
   },
