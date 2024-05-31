cd75e861faf490c59358210bb7d7a3be00f50713
Andrej Nikolic <andrej.nikolic@bouvet.no>
2024-05-31 13:57:19 +0200

    fix(ag-styles): additional data

--- packages/ag-grid-styles/src/agGridStyles/useAgGridStyles.ts
+++ packages/ag-grid-styles/src/agGridStyles/useAgGridStyles.ts
@@ -1,14 +1,9 @@
-import { tokens } from '@equinor/eds-tokens';
 import { ClassNameMap, createStyles, makeStyles } from '@equinor/fusion-react-styles';
 import styles from './styles.jss.json';
 
 export const useStyles = makeStyles(
   () =>
     createStyles({
-      root: () => ({
-        '--ag-background-color': tokens.colors.interactive.table__cell__fill_resting.hex,
-        '--ag-header-background-color': tokens.colors.interactive.table__header__fill_resting.hex,
-      }),
       ...styles,
     }),
   { name: 'fusion-ag-grid-styles' },
