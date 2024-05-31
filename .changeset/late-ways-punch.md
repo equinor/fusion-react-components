c0f774ced0f8f334778bce67e172d2091f0fbe80
Andrej Nikolic <andrej.nikolic@bouvet.no>
2024-05-31 08:12:38 +0200

    fix(ag-style): style cleanup and checkbox fix

---
+++ .changeset/late-ways-punch.md
@@ -0,0 +1,6 @@
+---
+'@equinor/fusion-react-ag-grid-styles': patch
+---
+
+- Cleanup AgGrid style with adding `alpine-active-color` to main style theme
+- In order to fix not beeing able to click on checkbox in AgGrid, removed `indicateEditMode` from **useStyle**
--- packages/ag-grid-styles/src/agGridStyles/useAgGridStyles.ts
+++ packages/ag-grid-styles/src/agGridStyles/useAgGridStyles.ts
@@ -1,27 +1,15 @@
-import { makeStyles, createStyles, ClassNameMap } from '@equinor/fusion-react-styles';
+import { tokens } from '@equinor/eds-tokens';
+import { ClassNameMap, createStyles, makeStyles } from '@equinor/fusion-react-styles';
 import styles from './styles.jss.json';
 
-type agGridProps = {
-  indicateEditMode?: boolean;
-};
-
 export const useStyles = makeStyles(
-  (theme) =>
+  () =>
     createStyles({
-      ...styles,
-      root: (props?: agGridProps) => ({
-        '--ag-row-hover-color': theme.colors.interactive.table__cell__fill_hover.getVariable('color'),
-        '--ag-selected-row-background-color': theme.colors.interactive.table__cell__fill_activated.getVariable('color'),
-        '--ag-background-color': theme.colors.interactive.table__cell__fill_resting.getVariable('color'),
-        '--ag-header-background-color': theme.colors.interactive.table__header__fill_resting.getVariable('color'),
-        '--ag-header-cell-hover-background-color':
-          theme.colors.interactive.table__header__fill_hover.getVariable('color'),
-        '--ag-header-cell-moving-background-color':
-          theme.colors.interactive.table__header__fill_activated.getVariable('color'),
-        '& :not(.inline-edit-mode)': {
-          opacity: `${props?.indicateEditMode ? '0.8' : '1'}`,
-        },
+      root: () => ({
+        '--ag-background-color': tokens.colors.interactive.table__cell__fill_resting.hex,
+        '--ag-header-background-color': tokens.colors.interactive.table__header__fill_resting.hex,
       }),
+      ...styles,
     }),
   { name: 'fusion-ag-grid-styles' },
 ) as () => ClassNameMap;
