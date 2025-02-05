80f579c8593077e3d8b93cb15f8ca5cec5b9b6d9
Andrej Nikolic <101325152+AndrejNikolicEq@users.noreply.github.com>
2025-02-05 14:39:54 +0100

    chore(ag-person-cell): ag grid 33 major update (#2433)
    * chore(ag-person-cell): ag grid 33 major update
    
    * fix(ag-person-cell): storybook

---
+++ .changeset/few-cows-search.md
@@ -0,0 +1,8 @@
+---
+'@equinor/fusion-react-ag-grid-person-cell': major
+---
+
+**Major update:** AgGrid version 33, possible braking changes
+
+- Updated types and dependencies to newest AgGrid version
+- Now using and depended on _@equinor/fusion-framework-module-ag-grid_
--- packages/ag-grid-person-cell/src/personTooltip.tsx
+++ packages/ag-grid-person-cell/src/personTooltip.tsx
@@ -1,13 +1,13 @@
-import { CustomTooltipProps } from '@ag-grid-community/react';
 import { PersonCard, PersonCellData } from '@equinor/fusion-react-person';
 
 type TooltipCard<T> = {
+  value: T;
   azureId?: (data: T) => string;
   upn?: (data: T) => string;
   dataSource?: (data: T) => PersonCellData;
 };
 
-export const personTooltip = <T,>(props: CustomTooltipProps & TooltipCard<T>): JSX.Element => {
+export const personTooltip = <T,>(props: TooltipCard<T>): JSX.Element => {
   const { value, azureId, upn, dataSource } = props;
   const azureResult = azureId ? azureId(value) : undefined;
   const upnResult = upn ? upn(value) : undefined;
